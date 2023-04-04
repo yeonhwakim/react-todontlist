8bac78cae18d68418c0020ae7b524b17046b6210
yeonhwa <yeonhwa6717@gmail.com>
2023-04-04 21:12:34 +0900

    feat: read 'to dont list'
    useState를 이용해서 상태관리
    dont list 조회하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [dontList, setDontList] = useState([
+    { id: 1, todo: "리액트 공부하기", startYn: false, doneYn: false },
+    { id: 2, todo: "설거지 하기", startYn: false, doneYn: false },
+    { id: 3, todo: "씻기", startYn: false, doneYn: false },
+  ]);
+
   return (
     <div className="App">
       <header className="App-header">TO DON'T LIST</header>
+      <ul>
+        {dontList.map(({ id, todo }) => (
+          <li key={id}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
