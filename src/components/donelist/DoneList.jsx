90ca8e1cc0bd17a2133d97d7a56f9517d229db06
yeonhwa <yeonhwa6717@gmail.com>
2023-04-09 23:13:11 +0900

    feat: Add actions related to 'donelist'
    Add actions related to the 'doneItem', including
    'deleted', 'updatedState', 'updatedTxt', 'canceledUpdate' and 'updated'
    Implement these actions in the code

--- src/components/donelist/DoneList.jsx
+++ src/components/donelist/DoneList.jsx
@@ -15,10 +15,40 @@ function DoneList() {
       .catch((e) => console.log(e));
   }, []);
 
+  const handleDelete = (deletedId) => {
+    dispatch({ type: "deleted", deletedId });
+  };
+
+  const handleUpdateState = (updatedId) => {
+    dispatch({ type: "updatedState", updatedId });
+  };
+
+  const handleUpdateTxt = (e, updatedId) => {
+    dispatch({ type: "updatedTxt", updatedId, value: e.target.value });
+  };
+
+  const handleCancelUpdate = (canceledId) => {
+    dispatch({ type: "canceledUpdate", canceledId });
+  };
+
+  const handleUpdate = (e, updatedId) => {
+    e.preventDefault();
+    dispatch({ type: "updated", updatedId });
+  };
+
   return (
     <div>
       <Header title={"DONE LIST"} />
-      {donelist && <List list={donelist} />}
+      {donelist && (
+        <List
+          list={donelist}
+          handleDelete={handleDelete}
+          handleUpdateState={handleUpdateState}
+          handleUpdate={handleUpdate}
+          handleUpdateTxt={handleUpdateTxt}
+          handleCancelUpdate={handleCancelUpdate}
+        />
+      )}
     </div>
   );
 }
