156e2458b8f6142051ddb2a3b6bc1684a21e63af
yeonhwa <yeonhwa6717@gmail.com>
2023-04-09 23:02:22 +0900

    feat: Add a 'fetch' action.
    Add a 'fetch' action that filters the 'todolist' array base on the
    'startYn' and 'doneYn' properties of each item to retrieve onley completed items

--- src/components/donelist/DoneList.jsx
+++ src/components/donelist/DoneList.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import Header from "../header/Header";
+import donelistReducer from "../../reducers/donelist-reducer";
+import List from "../list/List";
 
 function DoneList() {
+  const [donelist, dispatch] = useReducer(donelistReducer, []);
+
+  useEffect(() => {
+    fetch(`data/todos.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "fetch", todos: data });
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <div>
       <Header title={"DONE LIST"} />
+      {donelist && <List list={donelist} />}
     </div>
   );
 }
--- src/reducers/donelist-reducer.js
+++ src/reducers/donelist-reducer.js
@@ -1,5 +1,9 @@
 export default function donelistReducer(donelist, action) {
   switch (action.type) {
+    case "fetch": {
+      const { todos } = action;
+      return [...todos.filter((item) => item.startYn && item.doneYn)];
+    }
     default: {
       throw Error("Invalid action type");
     }
