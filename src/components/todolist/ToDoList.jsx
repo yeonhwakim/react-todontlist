e2024c7a732836e88eb75a9c9b99577b8ac2bf34
yeonhwa <yeonhwa6717@gmail.com>
2023-04-09 22:27:49 +0900

    feat: Add actions related to the 'todoItem'
    Add actions related to the 'todoItem', including
    'deleted', 'updatedState', 'updatedTxt', 'canceledUpdate' and 'updated'
    Implement these actions in the code

--- src/components/todolist/ToDoList.jsx
+++ src/components/todolist/ToDoList.jsx
@@ -14,10 +14,41 @@ function ToDontList() {
       })
       .catch((e) => console.log(e));
   }, []);
+
+  const handleDelete = (deletedId) => {
+    dispatch({ type: "deleted", deletedId });
+  };
+
+  const handleUpdateState = (updatedId) => {
+    dispatch({ type: "updatedState", updatedId });
+  };
+
+  const handleUpdateTxt = (e, updatedId) => {
+    dispatch({ type: "updatedTxt", updatedId, value: e.target.value });
+  };
+
+  const handleCancelUpdate = (canceledId) => {
+    dispatch({ type: "canceledUpdate", canceledId });
+  };
+
+  const handleUpdate = (e, updatedId) => {
+    e.preventDefault();
+    dispatch({ type: "updated", updatedId });
+  };
+
   return (
     <div>
       <Header title={"TO DO LIST"} />
-      {todolist && <List list={todolist} />}
+      {todolist && (
+        <List
+          list={todolist}
+          handleDelete={handleDelete}
+          handleUpdateState={handleUpdateState}
+          handleUpdate={handleUpdate}
+          handleUpdateTxt={handleUpdateTxt}
+          handleCancelUpdate={handleCancelUpdate}
+        />
+      )}
     </div>
   );
 }
