964129f036b2c57f52fe6dfa1b1270037bcf3a3f
yeonhwa <yeonhwa6717@gmail.com>
2023-04-09 22:56:11 +0900

    feat: Create a 'DoneList' compnent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import ToDontList from "./components/todontlist/ToDontList";
 import ToDoList from "./components/todolist/ToDoList";
+import DoneList from "./components/donelist/DoneList";
 
 function App() {
   return (
     <div className="App">
       <ToDontList />
       <ToDoList />
+      <DoneList />
     </div>
   );
 }
---
+++ src/components/donelist/DoneList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../header/Header";
+
+function DoneList() {
+  return (
+    <div>
+      <Header title={"DONE LIST"} />
+    </div>
+  );
+}
+
+export default DoneList;
