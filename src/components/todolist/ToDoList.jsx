7374dd6470a692cf7ce2e519a306bc9a1b197836
yeonhwa <yeonhwa6717@gmail.com>
2023-04-09 22:14:44 +0900

    feat: Add a 'fetch' action
    Add a 'fetch' action to the 'todolistReducer' and
    initialize the 'todolist' values
    Import the 'List' component and pass the 'todolist' prop as 'list'

--- src/components/todolist/ToDoList.jsx
+++ src/components/todolist/ToDoList.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useReducer } from "react";
+import todolistReducer from "../../reducers/todolist-reducer";
 import Header from "../header/Header";
+import List from "../list/List";
 
 function ToDontList() {
+  const [todolist, dispatch] = useReducer(todolistReducer, []);
+
+  useEffect(() => {
+    fetch(`data/todos.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "fetch", todos: data });
+      })
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <div>
-      <Header title={"TO DON'T LIST"} />
+      <Header title={"TO DO LIST"} />
+      {todolist && <List list={todolist} />}
     </div>
   );
 }
