32f4bab8fa2587d4ca2a3c5a9f6f2b6e1226b2ed
yeonhwa <yeonhwa6717@gmail.com>
2023-04-10 22:42:58 +0900

    feat: Create a 'NabBar' component.
    Create a new 'NabBar' component.
    Implement the 'Link' component from 'react-router-dom' to allow rendering of components
    base on the URL route when a tab is clicked.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Outlet />
     </div>
   );
