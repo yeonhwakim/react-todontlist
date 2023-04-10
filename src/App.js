874f43449847ddbd9c2710b25c4e7019c8dfe81d
yeonhwa <yeonhwa6717@gmail.com>
2023-04-10 22:21:37 +0900

    feat: Implement router functionality.
    Import 'createBrowserRouter' and 'RouterProvider' from 'react-router-dom'
    to enable routing functionality within the application.
    Then, add router paths as children within the root path in the 'index.js' file.
    Import 'Outlet' from 'react-router-dom' and use it to render components base on the current URL route.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import ToDontList from "./components/todontlist/ToDontList";
-import ToDoList from "./components/todolist/ToDoList";
-import DoneList from "./components/donelist/DoneList";
 
 function App() {
   return (
     <div className="App">
-      <ToDontList />
-      <ToDoList />
-      <DoneList />
+      <Outlet />
     </div>
   );
 }
