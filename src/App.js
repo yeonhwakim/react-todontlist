3eed62b17b65c17be1a6136f8a5628703127c786
yeonhwa <yeonhwa6717@gmail.com>
2023-05-04 21:02:12 +0900

    feat: Implement 'react-query' and devTools.
    Import QueryClient and QueryClientProvider from '@tanstack/react-query'.
    Create the 'queryClient' and Implement the 'QueryClientProvider' component with client props.
    Import the 'ReactQueryDevtools' from '@tanstack/react-query-devtools'.
    Implement the 'ReactQueryDevtools' component.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import NavBar from "./components/navbar/NavBar";
 import { createContext } from "react";
 import { DarkModeProvider } from "./contexts/DarkModeContext";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create a client
+const queryClient = new QueryClient();
 export const DarkModeContext = createContext();
 
 function App() {
   return (
-    <DarkModeProvider>
-      <div className="App">
-        <div className="navbar-wrapper">
-          <NavBar />
+    <QueryClientProvider client={queryClient}>
+      <DarkModeProvider>
+        <div className="App">
+          <div className="navbar-wrapper">
+            <NavBar />
+          </div>
+          <div className="list-wrapper">
+            <Outlet />
+          </div>
         </div>
-        <div className="list-wrapper">
-          <Outlet />
-        </div>
-      </div>
-    </DarkModeProvider>
+      </DarkModeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
